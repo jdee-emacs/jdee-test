f34a7079ecbedca60c8726012ef0bc947d59720a
Matthew O. Smith <matthew.smith@imail.org>
2016-11-20 08:51:49 -0700

    make sample more complex

--- src/main/java/com/github/jdee_test/issues/GH_80.java
+++ src/main/java/com/github/jdee_test/issues/GH_80.java
@@ -6,8 +6,11 @@ import java.util.regex.Pattern;
  * @see <a href="https://github.com/jdee-emacs/jdee/issues/80">gh-80</a>
  */
 
-public class Gh80 {
+public class GH_80 {
     String test = "['']";
+
     final static private Pattern pattFlName = Pattern.compile("([-'A-Za-z .]+)[ ,;]+([-'A-Za-z]+)$");
     final static private Pattern pattLfName = Pattern.compile("^([-'A-Za-z]+)[ ,;]+([-'A-Za-z .]+)");
+    
 }
+
